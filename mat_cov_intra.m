16f47d5ec800ff6c9f5d6e8eb6729e4b2f3c1014
onajibb <71511227+onajibb@users.noreply.github.com>
2022-04-05 17:33:00 +0200

    Delete mat_cov_intra.m

--- mat_cov_intra.m
+++
@@ -1,9 +0,0 @@
-function [sigma] = mat_cov_intra(data,moyenne,n)
-    taille = size(data);    
-    somme = zeros(taille(1),taille(1)); 
-    for i=1:taille(2)
-        somme = somme + ((data(:,i) - moyenne)*(data(:,i) -moyenne)'); 
-    end
-    sigma = somme; 
-end
-
